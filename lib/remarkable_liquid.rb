763745128094485749cce74e93d8b2724278ed00
Diego Carrion <dc.rec1@gmail.com>
2009-05-18 17:04:11 -0300

    first version

---
+++ lib/remarkable_liquid.rb
@@ -0,0 +1,27 @@
+module Remarkable
+  module Liquid
+    module Matchers
+      class HaveLiquidMethodsMatcher < Remarkable::ActiveRecord::Base
+        arguments :names
+        
+        assertion :has_liquid_method?
+        
+        def has_liquid_method?
+          begin
+            drop = eval("#{@subject.class}::LiquidDropClass.new")
+            @names.each do |name|
+              return false unless drop.respond_to? name
+            end
+          rescue
+            false
+          end
+        end
+      end
+
+      def have_liquid_methods(*attributes)
+        HaveLiquidMethodsMatcher.new(attributes).spec(self)
+      end
+
+    end
+  end
+end
---
+++ spec/remarkable_liquid_spec.rb
@@ -0,0 +1,28 @@
+require File.join(File.dirname(__FILE__), "spec_helper.rb")
+
+require 'liquid'
+
+create_table "companies" do end
+
+class Company < ActiveRecord::Base
+end
+
+class Company::LiquidDropClass < Liquid::Drop
+  def name
+  end
+  
+  def slogan
+  end
+end
+
+describe Remarkable::Liquid do
+  describe "have_liquid_methods" do
+    it "should validate that a model has liquid methods" do
+      have_liquid_methods(:name, :slogan).matches?(Company.new).should be_true
+    end
+    
+    it "should validate that a model doens't has a name" do
+      have_liquid_methods(:year).matches?(Company.new).should be_false
+    end
+  end
+end
